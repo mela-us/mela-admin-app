d61bc1a9e77b42deb387905d6ac5b3f3afa565ad
TEIKHNA <anhkiet07012003@gmail.com>
2025-07-14 13:05:25 +0700

    update base url

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,1 +1,1 @@
-export const BASE_URL = 'http://localhost:8080' || process.env.REACT_APP_BACKEND_URL || 'https://api.uat.mela.guru';
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL || 'https://api.uat.mela.guru';
